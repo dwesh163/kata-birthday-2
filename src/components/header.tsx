f67fd60bd091eea059e5a482cef20b798f031182
dwesh163 <ehouarn.duriaux@protonmail.com>
2024-08-28 11:26:50 +0200

    refactor: Update header component with new logo and styling

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,14 +1,17 @@
 import React from 'react';
 import { Button } from '@/components/ui/button';
-import { CalendarIcon, PlusIcon } from 'lucide-react';
+import { PlusIcon } from 'lucide-react';
+import LanguageSelector from '@/components/languageSelector';
 
 export function Header() {
 	return (
-		<header className="bg-primary text-primary-foreground py-4 px-6 flex items-center justify-between">
-			<div className="flex items-center gap-4">
-				<CalendarIcon className="w-6 h-6" />
-				<h1 className="text-2xl font-bold">Birthminder</h1>
+		<header className="text-primary-secondary sm:py-4 sm:px-6 py-2 px-3 flex items-center justify-between border-b-2 border-0">
+			<div className="flex items-center sm:gap-4 gap-3 sm:p-3 p-2">
+				<img src="https://epfl-si.github.io/elements/svg/epfl-logo.svg" alt="Birthminder" className="sm:h-7 h-5" />
+				<span className="border-l-2 border-solid h-6 w-1 border-gray-300"></span>
+				<h1 className="sm:text-2xl text-xl font-semibold">Birthminder</h1>
 			</div>
+			<LanguageSelector />
 			<Button variant="ghost">
 				<PlusIcon className="w-5 h-5" />
 				<span className="sr-only">Add Team Member</span>
