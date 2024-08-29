5dc4c532144b29b305d0ad490e6c04b3ab3c60f9
dwesh163 <ehouarn.duriaux@protonmail.com>
2024-08-29 17:38:07 +0200

    refactor: Update LanguageSelector component to use buttons instead of select dropdown

--- src/components/languageSelector.tsx
+++ src/components/languageSelector.tsx
@@ -1,13 +1,16 @@
+'use client';
 import React, { useEffect, useState } from 'react';
 import { setLocaleCookie } from '@/lib/cookie';
+import { useSession } from 'next-auth/react';
 
 const getCookieValue = (name: string): string | undefined => {
 	const match = document.cookie.match(new RegExp('(^| )' + name + '=([^;]+)'));
 	return match ? match[2] : undefined;
 };
 
-const LanguageSelector: React.FC = () => {
+export const LanguageSelector: React.FC = () => {
 	const [locale, setLocale] = useState<string>('en');
+	const { status } = useSession();
 
 	useEffect(() => {
 		const cookieLocale = getCookieValue('locale');
@@ -16,20 +19,28 @@ const LanguageSelector: React.FC = () => {
 		}
 	}, []);
 
-	const handleLocaleChange = (event: React.ChangeEvent<HTMLSelectElement>): void => {
-		const selectedLocale = event.target.value;
-		setLocaleCookie(selectedLocale);
-		setLocale(selectedLocale);
+	const changeLocale = (newLocale: string): void => {
+		setLocaleCookie(newLocale);
+		setLocale(newLocale);
 
 		window.location.reload();
 	};
 
 	return (
-		<select onChange={handleLocaleChange} value={locale}>
-			<option value="en">English</option>
-			<option value="fr">Français</option>
-		</select>
+		<nav className="ml-auto flex items-center space-x-4">
+			<ul className="flex items-center space-x-1">
+				<li>
+					<button onClick={() => changeLocale('fr')} className={`cursor-pointer font-bold ${locale === 'fr' && status != 'loading' ? 'text-red-500' : 'hover:text-gray-400 text-gray-300'}`}>
+						FR
+					</button>
+				</li>
+				<span className="border-l-2 border-solid h-4 w-1 border-gray-300"></span>
+				<li>
+					<button onClick={() => changeLocale('en')} className={`cursor-pointer -ml-0.5 font-bold ${locale === 'en' && status != 'loading' ? 'text-red-500' : 'hover:text-gray-400 text-gray-300'}`}>
+						EN
+					</button>
+				</li>
+			</ul>
+		</nav>
 	);
 };
-
-export default LanguageSelector;
