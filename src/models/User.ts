2cc07b6a025c6ea6935d8ebcaa008c01aa822011
dwesh163 <ehouarn.duriaux@protonmail.com>
2024-08-29 17:37:59 +0200

    feat: Add id, username, jobTitle, sciper, and image fields to User model

--- src/models/User.ts
+++ src/models/User.ts
@@ -3,10 +3,15 @@ import mongoose from 'mongoose';
 const { Schema } = mongoose;
 
 const UserSchema = new Schema({
+	id: String,
 	name: String,
+	username: String,
+	jobTitle: String,
 	email: String,
 	birthday: Date,
 	unit: String,
+	sciper: String,
+	image: String,
 });
 
 export const User = mongoose.models.User || mongoose.model('User', UserSchema);
