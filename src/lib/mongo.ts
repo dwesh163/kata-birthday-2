01c8dad060266a380ec6fc7563fe777ee96a0c13
dwesh163 <ehouarn.duriaux@protonmail.com>
2024-09-05 16:14:19 +0200

    [Update] MongoDB connection logic to prevent unnecessary connections

--- src/lib/mongo.ts
+++ src/lib/mongo.ts
@@ -2,5 +2,8 @@ import mongoose from 'mongoose';
 
 const url = `mongodb://${process.env.MONGO_USER_USERNAME}:${process.env.MONGO_USER_PASSWORD}@${process.env.MONGO_HOST}:27017/${process.env.MONGO_DATABASE}?directConnection=true`;
 export default async function connectDB() {
+	if (mongoose.connection.readyState >= 1) {
+		return;
+	}
 	await mongoose.connect(url);
 }
