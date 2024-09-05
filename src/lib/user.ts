d8078d41209e425c0dd18311aa6a5482412676aa
dwesh163 <ehouarn.duriaux@protonmail.com>
2024-09-05 16:13:01 +0200

    [Add] getUser function to fetch user profile by email

--- src/lib/user.ts
+++ src/lib/user.ts
@@ -12,3 +12,35 @@ export async function fetchUserProfile(accessToken: string) {
 	}
 	return null;
 }
+
+export async function getUser(token: string, email: string) {
+	const response = await fetch(`${process.env.MICROSOFT_GRAPH_API_URL}/users/${email}?$select=employeeId,id,displayName,jobTitle,mail,surname,userPrincipalName,birthday,companyName,joinedTeams,department`, {
+		method: 'GET',
+		headers: {
+			Authorization: `Bearer ${token}`,
+			'Content-Type': 'application/json',
+			ConsistencyLevel: 'eventual',
+		},
+	});
+
+	if (!response.ok) {
+		console.error('Failed to fetch user:', response.statusText);
+		return null;
+	}
+
+	const userProfile = await response.json();
+
+	if (userProfile) {
+		return {
+			sciper: userProfile.employeeId,
+			id: userProfile.id,
+			name: userProfile.displayName,
+			jobTitle: userProfile.jobTitle,
+			email: userProfile.mail || userProfile.userPrincipalName,
+			username: userProfile.surname.toLowerCase(),
+			birthday: userProfile.birthday,
+			unit: userProfile.department ? userProfile.department : userProfile.companyName === 'EPFL' ? 'EPFL' : 'Hors EPFL',
+		};
+	}
+	return null;
+}
